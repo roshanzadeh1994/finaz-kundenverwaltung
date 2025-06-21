cc5540f47a22b533a35792c3e9af50c293f06991
roshanzadeh1994 <roshanzadeh1994@yahoo.com>
2025-06-21 20:57:43 +0200

    debug dockerize

---
+++ backend/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+echo "Waiting for MySQL..."
+until nc -z -v -w30 db 3306
+do
+  echo "Waiting for database connection..."
+  sleep 5
+done
+
+
+php artisan config:clear
+php artisan migrate --force
+php artisan key:generate
+
+
+apache2-foreground
