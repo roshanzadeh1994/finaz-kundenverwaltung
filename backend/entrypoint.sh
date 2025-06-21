f3bc8b5530e6b68beca672251935f474465c8fa8
roshanzadeh1994 <roshanzadeh1994@yahoo.com>
2025-06-22 01:18:44 +0200

    Finalize working project setup with Docker, Laravel & Vue

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-
+# Warten auf MySQL
 echo "Waiting for MySQL..."
 until nc -z -v -w30 db 3306
 do
@@ -8,10 +8,10 @@ do
   sleep 5
 done
 
-
+# Laravel vorbereiten
 php artisan config:clear
 php artisan migrate --force
 php artisan key:generate
 
-
+# Apache starten
 apache2-foreground
